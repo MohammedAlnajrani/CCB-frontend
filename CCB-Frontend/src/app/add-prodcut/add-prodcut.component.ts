b958e4c7a11871eccf34174be7a103c97d72b69f
MajeedReact <mjalsalhi@gmail.com>
2022-10-27 22:54:25 +0300

    Added product linked to backend

--- CCB-Frontend/src/app/add-prodcut/add-prodcut.component.ts
+++ CCB-Frontend/src/app/add-prodcut/add-prodcut.component.ts
@@ -1,5 +1,10 @@
 import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormBuilder } from '@angular/forms';
+import { Router } from '@angular/router';
 import { product } from '../Model/product/product';
+import { AuthCustomerService } from '../services/auth-customer.service';
+import { AuthService } from '../services/customer/auth.service';
+import { ProductService } from '../services/product/product.service';
 
 @Component({
   selector: 'app-add-prodcut',
@@ -7,6 +12,7 @@ import { product } from '../Model/product/product';
   styleUrls: ['./add-prodcut.component.css'],
 })
 export class AddProdcutComponent implements OnInit {
+  form!: FormGroup;
   product: product = {
     product_name: '',
     product_quantity: 0,
@@ -20,7 +26,46 @@ export class AddProdcutComponent implements OnInit {
     seller_id: 0,
     category_id: 0,
   };
-  constructor() {}
+  token = this.authCustomer.getDecodedAccessToken(this.authCustomer.getToken());
+  sellerID = this.token.seller_id;
+  errorMsg = '';
+  added: boolean = false;
+  constructor(
+    private productService: ProductService,
+    private formBuilder: FormBuilder,
+    private router: Router,
+    private authCustomer: AuthCustomerService
+  ) {
+    this.form = this.formBuilder.group({
+      product_name: '',
+      product_quantity: '',
+      product_description: '',
+      tags: 2,
+      price: '',
+      lat: 0,
+      lan: 0,
+      city: '',
+      neighborhood: '',
+      seller_id: this.sellerID,
+      category_id: '',
+    });
+  }
 
   ngOnInit(): void {}
+
+  addProduct() {
+    this.productService.addProduct(this.form.getRawValue()).subscribe(
+      (data: any) => {
+        this.errorMsg = '';
+        this.added = true;
+
+        setTimeout(() => {
+          this.router.navigateByUrl(`/products/${data.product_id}`);
+        }, 1000);
+      },
+      (err) => {
+        this.errorMsg = err.error;
+      }
+    );
+  }
 }
